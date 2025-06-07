1aca73bd173350b2b7013331f31a5fd4863feddd
Torycd <vicidundun@gmail.com>
2025-06-07 15:38:11 +0100

    commit

--- 04-steps/steps/src/App-v1.js
+++ 04-steps/steps/src/App-v1.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import "./index.css";
 
 const messages = [
   "Learn React ⚛️",
@@ -11,7 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      {/* <Steps /> */}
+      <Steps />
     </div>
   );
 }
@@ -57,12 +56,12 @@ function Steps() {
           </p>
 
           <div className="buttons">
-            <Button
-              bgColor="#7950f2"
-              textColor="#fff"
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handlePrevious}
-              text="Previous"
-            />
+            >
+              Previous
+            </button>
             <button
               style={{ backgroundColor: "#7950f2", color: "#fff" }}
               onClick={handleNext}
@@ -73,16 +72,5 @@ function Steps() {
         </div>
       )}
     </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, text }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {text}
-    </button>
   );
 }
