b7c47d476fef8167a0fa203ed3c2ba8011ec6e8c
Torycd <vicidundun@gmail.com>
2025-06-09 17:11:39 +0100

    commit

---
+++ 06-eat-n-split/eat-n-split/src/components/Friend.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../index.css";
+import Button from "./Button";
+
+const Friend = ({ friend }) => {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} &#8358;{Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you &#8358;{Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+
+      <Button>Select</Button>
+    </li>
+  );
+};
+
+export default Friend;
