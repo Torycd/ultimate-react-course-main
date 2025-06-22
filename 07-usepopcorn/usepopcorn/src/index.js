2b11f3ba3d2d2231aac60297e0c3c198fc913f3a
Torycd <vicidundun@gmail.com>
2025-06-22 21:39:25 +0100

    commit

--- 07-usepopcorn/usepopcorn/src/index.js
+++ 07-usepopcorn/usepopcorn/src/index.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import StarRating from "./components/StarRating";
 // import "./index.css";
 // import App from "./App";
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0)
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "bad", "Okay", "good", "Amazing"]}
+    />
+    <StarRating size={24} color="red" className="test" defaultRating={3} />
     {/* <App /> */}
+    <Test />
   </React.StrictMode>
 );
